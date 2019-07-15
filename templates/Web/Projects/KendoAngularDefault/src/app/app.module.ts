41ac1da98216480708b06c32e2cc445e67e1855f
Veselin Veselinov Ivanov <veselin.ivanov@telerik.com>
2019-07-15 13:21:58 +0300

    Introduce Kendo Angular project template.

---
+++ templates/Web/Projects/KendoAngularDefault/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
