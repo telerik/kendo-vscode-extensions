b7abeb4997c74d174c4d442bb74dac9aea857e7b
Stefan M. Stefanov <ststefan@progress.com>
2019-12-26 11:35:51 +0200

    feat: update kendo react pages

--- templates/Web/Pages/KendoReact.Chart/src/components/Param_SourceName_Pascal.jsx
+++ templates/Web/Pages/KendoReact.Chart/src/components/Param_SourceName_Pascal.jsx
@@ -1,18 +1,21 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 import {
     Chart,
     ChartLegend,
     ChartSeries,
-    ChartSeriesItem
+    ChartSeriesItem,
+    ChartTitle,
+    ChartValueAxis,
+    ChartValueAxisItem
 } from '@progress/kendo-react-charts'
 
 const series = [
     { category: 'EUROPE', value: 0.3 },
-    { category: 'NORTH AMERIKA', value: 0.23 },
+    { category: 'NORTH AMERICA', value: 0.23 },
     { category: 'AUSTRALIA', value: 0.18 },
     { category: 'ASIA', value: 0.15 },
-    { category: 'SOUTH AMERIKA', value: 0.09 },
-    { category: 'AFRIC', value: 0.05 }
+    { category: 'SOUTH AMERICA', value: 0.09 },
+    { category: 'AFRICA', value: 0.05 }
 ];
 
 const Param_SourceName_Pascal = (props) => {
@@ -20,11 +23,16 @@ const Param_SourceName_Pascal = (props) => {
         <div className='container-fluid'>
             <div className='row my-4'>
                 <div className='col-12 col-lg-9 border-right'>
-                    <Chart>
+                    <Chart theme='sass' transitions={false}>
+                        {/*<ChartTooltip />  uncomment to enable Tooltips */}
+                        <ChartTitle text="Production" font="19pt sans-serif" margin={{ bottom: 50 }} />
                         <ChartLegend position="bottom" />
                         <ChartSeries>
-                            <ChartSeriesItem type="pie" data={series} field="value" categoryField="category" labels={{ visible: true, format: 'p0', position: 'auto', distance: 30 }} />
+                            <ChartSeriesItem type="column" data={series} field="value" categoryField="category" />
                         </ChartSeries>
+                        <ChartValueAxis>
+                            <ChartValueAxisItem labels={{ format: 'p0' }} />
+                        </ChartValueAxis>
                     </Chart>
                 </div>
                 <div className='col-12 col-lg-3 mt-3 mt-lg-0'>
@@ -32,6 +40,7 @@ const Param_SourceName_Pascal = (props) => {
                     <p>The KendoReact Charts provide high quality graphical data visualization options.</p>
                     <p>They include a variety of chart types and styles that have extensive configuration options. This flexibility allows you to quickly and easily create the exact chart you need to fit your specific requirements for functionality and appearance.</p>
                     <p>The KendoReact Chart Components are built from the ground up, specifically for React, so that you get high performance chart components that integrate with your React app and with the rest of your React UI components.</p>
+                    <p>For documentation and demos of all available Chart types (Bar, Line, Donut, Sparkline, StockChart etc), please visit the <a href="https://www.telerik.com/kendo-react-ui/components/charts/?utm_medium=cpm&utm_source=stackblitz-app&utm_campaign=kendo-ui-react-trial-charts">KendoReact Chart documentation page.</a> </p>
                 </div>
             </div>
         </div>
--- templates/Web/Pages/KendoReact.Form/src/components/Param_SourceName_Pascal.jsx
+++ templates/Web/Pages/KendoReact.Form/src/components/Param_SourceName_Pascal.jsx
@@ -1,75 +1,106 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
+import { Input, Checkbox } from '@progress/kendo-react-inputs';
+import { DatePicker } from '@progress/kendo-react-dateinputs';
+import { Form, Field } from '@progress/kendo-react-form';
+import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
+import FormContainer from './FormContainer';
+
+const emailRegex = new RegExp(/\S+@\S+\.\S+/);
+const emailValidator = (value) => (emailRegex.test(value) ? "" : "Please enter a valid email.");
+
+const EmailInput = (fieldRenderProps) => {
+    const { validationMessage, visited, ...others } = fieldRenderProps;
+    return (
+        <div>
+            <Input {...others} />
+            {
+                visited && validationMessage &&
+                (<div className={"k-required"}>{validationMessage}</div>)
+            }
+        </div>
+    );
+};
 
 const Param_SourceName_Pascal = (props) => {
+    const initialForm = {
+        firstName: '',
+        lastName: '',
+        dateOfBirth: new Date(),
+        email: '',
+        company: '',
+        userName: '',
+        password: '',
+        twoFactor: false
+    };
+    const [showDialog, setShowDialog] = useState(false)
+
+    const toggleDialog = () => {
+        setShowDialog(!showDialog);
+    }
+
+    const handleSubmit = () => {
+        setShowDialog(!showDialog);
+    }
+
     return (
         <div className="container-fluid">
+            {showDialog && <Dialog onClose={toggleDialog}>
+                <p style={{ margin: "25px", textAlign: "center" }}>The form is successfully submitted!</p>
+                <DialogActionsBar>
+                    <button className="k-button" onClick={toggleDialog}>OK</button>
+                </DialogActionsBar>
+            </Dialog>}
             <div className='row my-4'>
-                <div className='col-12 col-lg-9 border-right'>
-                    <div className="row example-wrapper">
-                        <div className="col-xs-12 col-sm-6 offset-sm-3 example-col">
-                            <div className="card">
-                                <div className="card-block">
-                                    <form className="k-form">
-                                        <fieldset>
-                                            <legend>User Details</legend>
+                <FormContainer>
+                    <Form
+                        initialValues={initialForm}
+                        onSubmit={handleSubmit}
+                        render={(formRenderProps) => (
+                            <form onSubmit={formRenderProps.onSubmit} className={'k-form'}>
+                                <fieldset>
+                                    <legend>User Details</legend>
+                                    <div>
+                                        <Field name={'firstName'} component={Input} label={'First name'} />
+                                    </div>
+                                    <div>
+                                        <Field name={'lastName'} component={Input} label={'Last name'} />
+                                    </div>
+                                    <div style={{ marginTop: "1rem" }}>
+                                        <Field name={'dateOfBirth'} component={DatePicker} label={'Date of Birth'} />
+                                    </div>
+                                    <div>
+                                        <Field name={"email"} type={"email"} component={EmailInput} label={"Email"} validator={emailValidator} />
+                                    </div>
+                                    <div>
+                                        <Field name={'company'} component={Input} label={'Your Company'} />
+                                    </div>
+                                </fieldset>
 
-                                            <label className="k-form-field">
-                                                <span>First Name</span>
-                                                <input className="k-textbox" placeholder="Your Name" />
-                                            </label>
-                                            <label className="k-form-field">
-                                                <span>Last Name</span>
-                                                <input className="k-textbox" placeholder="Your Last Name" />
-                                            </label>
-                                            <div className="k-form-field">
-                                                <span>Gender</span>
+                                <fieldset>
+                                    <legend>Credentials</legend>
+                                    <div>
+                                        <Field name={'userName'} component={Input} label={'Username'} placeholder="Your username" />
+                                    </div>
+                                    <div>
+                                        <Field name={'password'} component={Input} label={'Password'} placeholder="Your password" />
+                                    </div>
+                                    <div style={{ marginTop: "1rem" }}>
+                                        <Field name={'twoFactor'} component={Checkbox} label={'Enable two-factor authentication'} />
+                                    </div>
+                                </fieldset>
 
-                                                <input type="radio" name="gender" id="female" className="k-radio" />
-                                                <label className="k-radio-label" htmlFor="female">Female</label>
-
-                                                <input type="radio" name="gender" id="male" className="k-radio" />
-                                                <label className="k-radio-label" htmlFor="male">Male</label>
-                                            </div>
-                                            <label className="k-form-field">
-                                                <span>Email <span className="k-required">*</span></span>
-                                                <input type="email" className="k-textbox" placeholder="Your Email" />
-                                            </label>
-                                            <label className="k-form-field">
-                                                <span>Company<span className="k-field-info">optional</span></span>
-                                                <input className="k-textbox" placeholder="Your Company" />
-                                            </label>
-                                        </fieldset>
-
-                                        <fieldset>
-                                            <legend>Credentials</legend>
-                                            <label className="k-form-field">
-                                                <span>Username</span>
-                                                <input className="k-textbox" placeholder="Your username" />
-                                            </label>
-                                            <label className="k-form-field">
-                                                <span>Password</span>
-                                                <input type="password" className="k-textbox" placeholder="Your password" />
-                                            </label>
-                                            <label className="k-form-field">
-                                                <input type="checkbox" id="auth-2fa" className="k-checkbox" />
-                                                <label className="k-checkbox-label" htmlFor="auth-2fa">Enable two-factor authentication</label>
-                                            </label>
-                                        </fieldset>
-
-                                        <div className="text-right">
-                                            <button type="button" className="k-button">Cancel</button> &nbsp;
-                                <button type="button" className="k-button k-primary">Submit</button>
-                                        </div>
-                                    </form>
+                                <div className="text-right">
+                                    <button type="button" className="k-button" onClick={formRenderProps.onFormReset}>Clear</button> &nbsp;
+                                    <button type="submit" className="k-button k-primary" disabled={!formRenderProps.allowSubmit}>Submit</button>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                            </form>
+                        )} />
+                </FormContainer>
                 <div className='col-12 col-lg-3 mt-3 mt-lg-0'>
-                    <h3>KendoReact Chart</h3>
+                    <h3>KendoReact Forms</h3>
                     <p>KendoReact includes a wide offering of UI components that can be used to build forms, including CSS classes to easily create and structure gorgeous forms.</p>
-                    <p>All inputs get validated upon form submission and if the validation fails, the form submission is prevented. Out of the box, KendoReact delivers components which support the HTML5 form validation and also provide props for configuring a set of minimal requirements for a component to be in a valid state.</p>
+                    <p>The required inputs get validated upon form submission and if the validation fails, the form submission is prevented. Out of the box, KendoReact delivers components which support the HTML5 form validation and also provide props for configuring a set of minimal requirements for a component to be in a valid state.</p>
+                    <p>For documentation and demos of the many form-friendly components please visit their documentation (<a href="https://www.telerik.com/kendo-react-ui/components/dateinputs/?utm_medium=cpm&utm_source=stackblitz-app&utm_campaign=kendo-ui-react-trial-dateinputs">Date Inputs</a>, <a href="https://www.telerik.com/kendo-react-ui/components/dropdowns/?utm_medium=cpm&utm_source=stackblitz-app&utm_campaign=kendo-ui-react-trial-dropdowns">DropDowns</a>, <a href="https://www.telerik.com/kendo-react-ui/components/inputs/?utm_medium=cpm&utm_source=stackblitz-app&utm_campaign=kendo-ui-react-trial-inputs">Inputs</a> etc).</p>
                 </div>
             </div>
         </div>
