c65069f6370b33f337a483001b25f44004e31c8a
zdravkov <zdravkov.pl@gmail.com>
2020-01-22 15:46:04 +0200

    chore: add vue compositions

---
+++ templates/Web/_composition/KendoVue/Page.Kendo.Vue.AddFormContainer/src/components/FormContainer.vue
@@ -0,0 +1,13 @@
+<template>
+<div class='col-12 col-lg-9 border-right'>
+    <div class="row example-wrapper">
+        <div class="col-xs-12 col-sm-6 offset-sm-3 example-col">
+            <div class="card">
+                <div class="card-block">
+                   <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
---
+++ templates/Web/_composition/KendoVue/Page.Kendo.Vue.AddRoute/src/router/index_postaction.js
@@ -0,0 +1,16 @@
+﻿routes: [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  //^^
+  //{[{
+    {
+      path: "/",
+      name: "Param_SourceName_Pascal",
+      component: Param_SourceName_Pascal
+    },
+  //}]}
+]
+});
