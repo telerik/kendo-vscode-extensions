41409b24842e7238f7af41a749f9676726ca5512
zdravkov <zdravkov.pl@gmail.com>
2020-01-23 11:49:35 +0200

    chore: add correct license link

--- templates/Web/Projects/KendoVueDefault/src/components/Footer.vue
+++ templates/Web/Projects/KendoVueDefault/src/components/Footer.vue
@@ -3,7 +3,7 @@
             <div class='d-flex'>
                  <div>
                 <div class='mb-2 links'>
-                    <a href="https://www.telerik.com/purchase/license-agreement/progress-kendovue?utm_medium=cpm&utm_source=stackblitz-app&utm_campaign=kendo-ui-vue-awareness-license-agreement">License Agreement</a>
+                    <a href="https://www.telerik.com/purchase/license-agreement/kendo-ui?utm_medium=cpm&utm_source=vscode-app&utm_campaign=kendo-ui-vue-awareness-license-agreement">License Agreement</a>
                 </div>
                 <div class='copyright'>
                     Copyright © {{currentYear}} Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
