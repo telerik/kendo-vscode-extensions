aace6f64636172162fa355f64c0fce2796c5c98e
Nikola Nenov <nikola.nenoff@gmail.com>
2020-06-23 15:41:37 +0300

    Updated templates for version 2.15 (#58)

--- blazor-ui-template-wizard/templates/Web/_composition/BlazorClient/Blazor.Client.AddWeatherService/Shared/WeatherForecast.cs
+++ blazor-ui-template-wizard/templates/Web/_composition/BlazorClient/Blazor.Client.AddWeatherService/Shared/WeatherForecast.cs
@@ -8,9 +8,30 @@ namespace ClientApp.Shared
 
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        private double _tempC { get; set; }
+        public double TemperatureC
+        {
+            get
+            {
+                return _tempC;
+            }
+            set
+            {
+                _tempC = value;
+            }
+        }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public double TemperatureF
+        {
+            get
+            {
+                return 32 + (_tempC / 0.5556);
+            }
+            set
+            {
+                _tempC = (value - 32) * 0.5556;
+            }
+        }
 
         public string Summary { get; set; }
 
